acbbeb1277c2a9704c261e7176a6bfcab4ff0449
Luis Felipe Martínez(Kispat) <martinezbluisf@gmail.com>
2026-04-17 20:06:17 -0500

    Update utils.py

--- organizer/utils.py
+++ organizer/utils.py
@@ -1,1 +1,14 @@
+import os
+
+def is_valid_file(path):
+    return os.path.isfile(path)
+
+def get_extension(filename):
+    if "." in filename:
+        return filename.split('.')[-1].lower()
+    return ""
+
+def ensure_directory(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
 
